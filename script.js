9577bb9c5d0950f93713c8c70072629a891a15b3
Abinesh M <abineshm03@gmail.com>
2026-01-31 17:25:01 +0530

    Abinesh portfolio initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const roles = [
+  "Software Developer",
+  "Full Stack Developer",
+  "Front End Developer",
+  "Back End Developer",
+  "Web Developer"
+];
+
+let index = 0;
+const role = document.getElementById("role");
+
+setInterval(() => {
+  role.style.animation = "none";
+  role.offsetHeight;
+
+  index = (index + 1) % roles.length;
+  role.textContent = roles[index];
+
+  role.style.animation = "rotateText 2s ease-in-out, colorShift 6s infinite";
+}, 2200);
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
